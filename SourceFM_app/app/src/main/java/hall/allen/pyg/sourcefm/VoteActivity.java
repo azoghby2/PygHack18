af09f023a855fe4dd874f813c91cb2a310d8628e
beezow <zbeezow@gmail.com>
2018-09-29 14:50:02 -0500

    can show a song using xml layout

--- SourceFM_app/app/src/main/java/hall/allen/pyg/sourcefm/VoteActivity.java
+++ SourceFM_app/app/src/main/java/hall/allen/pyg/sourcefm/VoteActivity.java
@@ -1,7 +1,12 @@
 package hall.allen.pyg.sourcefm;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class VoteActivity extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class VoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vote);
+
+
+
+        LayoutInflater vi = (LayoutInflater)
+                this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View song = vi.inflate(R.layout.song_item,null);
+        LinearLayout voteQueue = findViewById(R.id.vote_view);
+        ((TextView) song.findViewById(R.id.song_name)).setText("Song");
+        ((TextView) song.findViewById(R.id.song_singer)).setText("Singer");
+        ((TextView) song.findViewById(R.id.song_length)).setText("0:00");
+        ((TextView) song.findViewById(R.id.song_vote)).setText("121");
+        voteQueue.addView(song);
     }
+
+    
 }
